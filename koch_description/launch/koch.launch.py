4ce4fe964b740692d652e5930c60fb18e895ef4e
Zheng Qu <quzhengrobot@gmail.com>
2025-07-28 00:49:34 +0200

    [koch_description] Support ros2_control

---
+++ koch_description/launch/koch.launch.py
@@ -0,0 +1,70 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+import xacro
+
+
+def generate_launch_description():
+    share_dir = get_package_share_directory("koch_description")
+
+    xacro_file = os.path.join(share_dir, "urdf", "koch.xacro")
+    rviz_config = os.path.join(share_dir, "config", "display.rviz")
+
+    robot_description_config = xacro.process_file(xacro_file)
+
+    controller_config = os.path.join(share_dir, "config", "controllers.yaml")
+    print(f"Loading controller config from: {controller_config}")
+
+    return LaunchDescription(
+        [
+            Node(
+                package="controller_manager",
+                executable="ros2_control_node",
+                parameters=[
+                    {"robot_description": robot_description_config.toxml()},
+                    controller_config,
+                ],
+                output="screen",
+            ),
+            Node(
+                package="controller_manager",
+                executable="spawner",
+                arguments=[
+                    "joint_state_broadcaster",
+                    "--controller-manager",
+                    "/controller_manager",
+                ],
+                output="screen",
+            ),
+            Node(
+                package="controller_manager",
+                executable="spawner",
+                arguments=["velocity_controller", "-c", "/controller_manager"],
+                output="screen",
+            ),
+            Node(
+                package="controller_manager",
+                executable="spawner",
+                arguments=["joint_trajectory_controller", "-c", "/controller_manager"],
+                output="screen",
+            ),
+            Node(
+                package="robot_state_publisher",
+                executable="robot_state_publisher",
+                name="robot_state_publisher",
+                parameters=[{"robot_description": robot_description_config.toxml()}],
+                output="screen",
+            ),
+            Node(
+                package="rviz2",
+                executable="rviz2",
+                name="rviz2",
+                arguments=["-d", rviz_config],
+                output="screen",
+            ),
+        ]
+    )
