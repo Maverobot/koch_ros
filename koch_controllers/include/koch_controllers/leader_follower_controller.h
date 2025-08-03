450da62581b1e2a724f10c74bcad50f4a148fabb
Zheng Qu <quzhengrobot@gmail.com>
2025-08-03 16:06:24 +0200

    [koch_controllers] Add leader follower controller
    Fixes: https://github.com/Maverobot/koch_ros/issues/5

---
+++ koch_controllers/include/koch_controllers/leader_follower_controller.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "controller_interface/controller_interface.hpp"
+#include "hardware_interface/loaned_command_interface.hpp"
+#include "hardware_interface/loaned_state_interface.hpp"
+#include "rclcpp_lifecycle/node_interfaces/lifecycle_node_interface.hpp"
+
+#include <functional>
+
+namespace koch_controllers {
+
+class LeaderFollowerController
+    : public controller_interface::ControllerInterface {
+public:
+  controller_interface::CallbackReturn on_init() override;
+
+  controller_interface::CallbackReturn
+  on_configure(const rclcpp_lifecycle::State &previous_state) override;
+
+  controller_interface::CallbackReturn
+  on_activate(const rclcpp_lifecycle::State &previous_state) override;
+
+  controller_interface::return_type
+  update(const rclcpp::Time &time, const rclcpp::Duration &period) override;
+
+  controller_interface::InterfaceConfiguration
+  command_interface_configuration() const override;
+
+  controller_interface::InterfaceConfiguration
+  state_interface_configuration() const override;
+
+private:
+  std::vector<std::string> leader_joint_names_;
+  std::vector<std::string> follower_joint_names_;
+  std::vector<double> leader_to_follower_offset_;
+  std::vector<double> leader_to_follower_scale_;
+
+  std::vector<std::reference_wrapper<hardware_interface::LoanedStateInterface>>
+      leader_state_handles_;
+  std::vector<
+      std::reference_wrapper<hardware_interface::LoanedCommandInterface>>
+      follower_command_handles_;
+};
+
+} // namespace koch_controllers
---
+++ koch_controllers/launch/leader_follower.launch.py
@@ -0,0 +1,83 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+
+import xacro
+
+
+def generate_launch_description():
+    koch_description_share_dir = get_package_share_directory("koch_description")
+    koch_controllers_share_dir = get_package_share_directory("koch_controllers")
+
+    xacro_file = os.path.join(koch_description_share_dir, "urdf", "koch.xacro")
+    rviz_config = os.path.join(koch_description_share_dir, "config", "display.rviz")
+
+    robot_description_config = xacro.process_file(xacro_file)
+
+    controller_config = os.path.join(
+        koch_controllers_share_dir, "config", "koch_controllers.yaml"
+    )
+    print(f"Loading controller config from: {controller_config}")
+
+    # Declare a launch argument to decide whether to launch RViz
+    rviz_arg = DeclareLaunchArgument(
+        "rviz", default_value="true", description="Flag to enable RViz"
+    )
+
+    # Function to conditionally include RViz node
+    def launch_setup(context, *args, **kwargs):
+        use_rviz = LaunchConfiguration("rviz").perform(context) == "true"
+        nodes = [
+            Node(
+                package="controller_manager",
+                executable="ros2_control_node",
+                parameters=[
+                    {"robot_description": robot_description_config.toxml()},
+                    controller_config,
+                ],
+                output="screen",
+            ),
+            Node(
+                package="controller_manager",
+                executable="spawner",
+                arguments=[
+                    "joint_state_broadcaster",
+                    "--controller-manager",
+                    "/controller_manager",
+                ],
+                output="screen",
+            ),
+            Node(
+                package="controller_manager",
+                executable="spawner",
+                arguments=["leader_follower_controller", "-c", "/controller_manager"],
+                output="screen",
+            ),
+            Node(
+                package="robot_state_publisher",
+                executable="robot_state_publisher",
+                name="robot_state_publisher",
+                parameters=[{"robot_description": robot_description_config.toxml()}],
+                output="screen",
+            ),
+        ]
+
+        if use_rviz:
+            nodes.append(
+                Node(
+                    package="rviz2",
+                    executable="rviz2",
+                    name="rviz2",
+                    arguments=["-d", rviz_config],
+                    output="screen",
+                )
+            )
+
+        return nodes
+
+    return LaunchDescription([rviz_arg, OpaqueFunction(function=launch_setup)])
